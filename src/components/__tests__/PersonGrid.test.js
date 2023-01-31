f9e56066d9eacccec05625384db0cfc4477f2cff
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-01-31 09:06:38 +0100

    added test for PersonGrid

--- src/components/__tests__/PersonGrid.test.js
+++ src/components/__tests__/PersonGrid.test.js
@@ -1,6 +1,9 @@
 import {render, screen, cleanup } from '@testing-library/react';
 import PersonGrid from '../PersonGrid';
 
+afterEach(() => {
+    cleanup();
+})
 test ('should render PersonGrid component', () => {
     render(<PersonGrid/>);
     const element = screen.getByTestId('tid-1');
@@ -9,4 +12,6 @@ test ('should render PersonGrid component', () => {
     expect(element).toHaveTextContent("E-mail");
     expect(element).toHaveTextContent("Gender");
     expect(element).toHaveTextContent("IP address");
+
+    expect(element).toContainHTML("<table");
 })
