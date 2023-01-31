266a298424d4a64acefd4ba589ee7593d75703ce
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-01-31 16:21:11 +0100

    update

--- src/utils/logProfiler.js
+++ src/utils/logProfiler.js
@@ -1,3 +1,5 @@
 const logTimes = (id, phase, actualTime, baseTime, startTime, commitTime) => {
-    console.table({ id, phase, actualTime, baseTime, startTime, commitTime });
-  };
+  console.table({ id, phase, actualTime, baseTime, startTime, commitTime });
+};
+
+export { logTimes };
