11f16864b2e9aae9fc2bf6fe70cb74d03e69f4ff
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-01-31 11:39:28 +0100

    added 2 functional examples

--- src/components/PersonGridFilter.js
+++ src/components/PersonGridFilter.js
@@ -19,4 +19,24 @@ const PersonGridFilter = (props) => {
   )
 }
 
+// Functional version - pure function - props destructuring
+// const PersonGridFilter = ({ onFirstNameChange, onOnlyActiveChange }) => (
+//   <>
+//     <tr>
+//       <td><input type="text" onChange={onFirstNameChange} /></td>
+//       <td><input type="text" /></td>
+//       <td><input type="text" /></td>
+//       <td><input type="text" /></td>
+//       <td><input type="text" /></td>
+//     </tr>
+//     <tr>
+//       <td colSpan={5}>
+//         <label>
+//           <input type="checkbox" onChange={onOnlyActiveChange} /> Only active
+//         </label>
+//       </td>
+//     </tr>
+//   </>
+// );
+
 export default PersonGridFilter
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,8 +1,21 @@
+// const filterData = (data, firstNamePrefix, onlyActive) =>
+//   data.filter((item) => {
+//     const fnLowerTest = item.Name.toLowerCase().startsWith(firstNamePrefix);
+//     const activeTest = !onlyActive || (onlyActive && item.Is_active);
+//     return fnLowerTest && activeTest;
+//   });
+
+// Functional version (destructured)
 const filterData = (data, firstNamePrefix, onlyActive) =>
-  data.filter((item) => {
-    const fnLowerTest = item.Name.toLowerCase().startsWith(firstNamePrefix);
-    const activeTest = !onlyActive || (onlyActive && item.is_active);
-    return fnLowerTest && activeTest;
-  });
+  data
+    .map((item) => ({
+      ...item,
+      Name: item.Name.toLowerCase(),
+    }))
+    .filter(
+      (item) =>
+        item.Name.startsWith(firstNamePrefix.toLowerCase()) &&
+        (!onlyActive || item.is_active)
+    );
 
 export { filterData };
